33633546ea23e1d438a347adcc94fe7870703922
shitalmeshram <sh.meshram@gmail.com>
2017-04-24 00:47:52 -0700

    change in geckodriver new method

--- src/test/java/com/bidqa/test/LoginTest.java
+++ src/test/java/com/bidqa/test/LoginTest.java
@@ -3,13 +3,16 @@ import java.util.concurrent.TimeUnit;
 //--
 import org.openqa.selenium.By;
 //--
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 //--
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 //--
 import org.testng.annotations.AfterClass;
@@ -35,6 +38,7 @@ public class LoginTest {
     public void afterClass() throws InterruptedException {
 
        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+       Thread.sleep(5000);
        driver.quit();
     }
 
@@ -56,21 +60,37 @@ public class LoginTest {
 
     }
 @Test(dependsOnMethods = {"loginVerification"})
-public void subscribeVerification() {
-
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+public void subscribeVerification() throws InterruptedException {
 
 
-    WebElement element= driver.findElement(By.xpath(".//*[@id='category']"));
+   /* WebElement element= driver.findElement(By.xpath("./[@id='category']"));
 
     Actions action =new Actions(driver);
     action.moveToElement(element).build().perform();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.findElement(By.xpath(".//*[@id='category']/option[3]")).click();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+   // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    WebDriverWait wait = new WebDriverWait(driver, 5);
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("./[@id='category']/option[3]")));
+    driver.findElement(By.xpath("./*//*[@id='category']/option[3]")).click();
+   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);*/
 
-   // Select se=new Select(element);
-   // se.selectByVisibleText("Automation – Mobile");
+/*   Select se=new Select(element);
+   se.selectByVisibleText("Automation – Mobile");*/
+
+    Select dropdown1 = new Select(driver.findElement(By.id("category")));
+    dropdown1.selectByVisibleText("Black Box");
+    Thread.sleep(2000);
+
+    Select dropdown2 = new Select(driver.findElement(By.id("location")));
+    dropdown2.selectByVisibleText("Any location");
+
+    Thread.sleep(2000);
+    Select dropdown3 = new Select(driver.findElement(By.name("budgets")));
+    dropdown3.selectByVisibleText("Per Hour ($1 - $10)");
+    Thread.sleep(2000);
+    driver.findElement(By.xpath(".//*[@id='subscribe-form']/button")).click();
+
+
+    Thread.sleep(5000);
 
 }
   @Test(dependsOnMethods = {"subscribeVerification"})
@@ -79,5 +99,4 @@ public void logOut(){
         driver.findElement(By.xpath(".//*[@id='cssmenu']/ul/li[7]/a")).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 }
-
 }
